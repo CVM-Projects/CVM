19e0a6bb6c123f42be5ac28841a887fb06031721
Chill Magic <hanlengmowang@hotmail.com>
2019-05-11 20:16:23 +0800

    Move HashStringPool to PriLib

--- include/inststruct/hashstringpool.h
+++ include/inststruct/hashstringpool.h
@@ -1,70 +1,8 @@
 #pragma once
-#include <functional>
-#include <map>
-#include <cassert>
-#include "../prilib/include/explicittype.h"
-#include "../prilib/include/stringview.h"
+#include "../prilib/include/hashstringpool.h"
 
 namespace CVM
 {
-	DefineExplicitTypeWithValue(HashID, size_t, 0);
-
-	inline bool operator<(const HashID &lhs, const HashID &rhs) {
-		return lhs.data < rhs.data;
-	}
-
-	class HashStringPool
-	{
-	public:
-		class HashIDGetter {
-		public:
-			explicit HashIDGetter(const HashID &hashId, const HashStringPool &hashStringPool)
-				: hashId(hashId), hashStringPool(hashStringPool) {}
-			const std::string& operator()() {
-				return hashStringPool.get(hashId);
-			}
-
-		private:
-			const HashID &hashId;
-			const HashStringPool &hashStringPool;
-		};
-
-	public:
-		explicit HashStringPool() = default;
-
-		const std::string& get(const HashID &id) const {
-			return _data.at(id.data);
-		}
-
-		bool has(const HashID &id) const {
-			return _data.find(id.data) != _data.end();
-		}
-
-		HashIDGetter getIDGetter(const HashID &id) const {
-			return HashIDGetter(id, *this);
-		}
-
-		HashID getID(const std::string &value) const {
-			return HashID(_getHash(value));
-		}
-
-		HashID insert(const std::string &value) {
-			size_t id = getID(value).data;
-			if (_data.find(id) == _data.end()) {
-				_data.insert({ id, value });
-			}
-			else {
-				assert(_data.at(id) == value);
-			}
-			return HashID(id);
-		}
-
-		HashID insert(const PriLib::StringViewRange &value) {
-			return insert(value.toString());
-		}
-
-	private:
-		std::hash<std::string> _getHash;
-		std::map<size_t, std::string> _data;
-	};
+	using HashID = PriLib::HashStringPool::HashID;
+	using HashStringPool = PriLib::HashStringPool;
 }
